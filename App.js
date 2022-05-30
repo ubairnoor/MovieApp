1463d3ccbebc78d26e7d571b43d5e2f076e0007e
ubair <ubairnoor5@gmail.com>
2022-05-30 12:01:18 +0530

    JSON Stringfy Added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const getPopulsarMovies = async () =>{
   const Resp =  await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=283bf3f22f7ce6d2d1f9043a8b429b91')
-console.log("Movies Response :",Resp)
+console.log(JSON.stringify(Resp, null, 2))
 }
 
 const App = () =>{ 
