863f13e872a1e6793ccf0164077f9afe778ae1c7
ubair <ubairnoor5@gmail.com>
2022-05-30 11:57:25 +0530

    Axios Dependency added and Popular Movie Api Added

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Text,View} from 'react-native';
+import axios from 'axios';
 
+const getPopulsarMovies = async () =>{
+  const Resp =  await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=283bf3f22f7ce6d2d1f9043a8b429b91')
+console.log("Movies Response :",Resp)
+}
 
-const App = () =>{
+const App = () =>{ 
+  getPopulsarMovies();
   return (
     <View>
        <Text>Hello</Text>
