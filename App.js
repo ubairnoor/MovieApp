3b66fde26eec7edab6b948b56fd1f5db2a200375
ubair <ubairnoor5@gmail.com>
2022-05-31 09:29:54 +0530

    Gettting Data From Server issue fixed.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import    React,{ useState } from 'react';
+import    React,{ useState ,useEffect} from 'react';
 import {Text,View} from 'react-native';
 import axios from 'axios';
 
@@ -14,11 +14,19 @@ const App = () =>{
   // THe issue is that tjhe title is set after the template got initialized.
   // We cannot use Normal Variable we use React State.
   //  
+
+  // Important 
+  //>> After we are using useState our CPU is going out of resourse to get rid off from this.
+  // when we are using the state and async method we have to use UseEffect.
+  //
+
   const [ movie ,setMovie] = useState(''); 
-  getPopulsarMovies().then(movies => {
-    setMovie(movies[0]);
- 
-  });
+  useEffect(()=>{
+    getPopulsarMovies().then(movies => {
+      setMovie(movies[0]);
+   
+    },[]);
+  })
   return (
     <View>
        <Text>Movie Title: {movie.original_title}</Text>
