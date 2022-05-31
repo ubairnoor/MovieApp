ad869ed06936546ff872c5897773d158956552dc
ubair <ubairnoor5@gmail.com>
2022-05-31 11:41:04 +0530

    Error Catch Added

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import    React,{ useState ,useEffect} from 'react';
-import {Text,View} from 'react-native';
+import { Text , View} from 'react-native';
 import axios from 'axios';
 
 const getPopulsarMovies = async () =>{
   const Resp =  await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=283bf3f22f7ce6d2d1f9043a8b429b91')
 // console.log(JSON.stringify(Resp.data.results, null, 2))
-return Resp.data.results;
+    return Resp.data.results;
 }
 
 const App = () =>{  
@@ -19,18 +19,22 @@ const App = () =>{
   //>> After we are using useState our CPU is going out of resourse to get rid off from this.
   // when we are using the state and async method we have to use UseEffect.
   //
+// To catch the error we use Catch to  get Error from API.
 
   const [ movie ,setMovie] = useState(''); 
+  const [ error, setError] = useState(false);
   useEffect(()=>{
     getPopulsarMovies().then(movies => {
       setMovie(movies[0]);
-   
-    },[]);
-  })
+    }).catch(err => {
+      setError(err);
+    });
+  },[])
   return (
     <View>
-       <Text>Movie Title: {movie.original_title}</Text>
-       <Text>Released On: {movie.release_date}</Text>
+       <Text>Movie Title:  {movie.original_title}</Text>
+       <Text>Released On:  {movie.release_date}</Text>
+     { error && <Text style={{ color:'red'}}> Error in the Server. </Text>  }
     </View>
   )
 }
