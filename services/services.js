583331e75eaaca2e1e482495e120e5480b6d48cc
ubair <ubairnoor5@gmail.com>
2022-05-31 17:02:54 +0530

    Error Fixed

--- services/services.js
+++ services/services.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
 
-const ApiUrl = 'https://api.themoviedb.org'
-const ApiKey = '283bf3f22f7ce6d2d1f9043a8b429b91'
+const ApiUrl = 'https://api.themoviedb.org/3'
+const ApiKey = 'api_key=283bf3f22f7ce6d2d1f9043a8b429b91'
 export  const getPopularMovies =  async () => {
-    const resp =await axios.get(`${ApiUrl}/3/movie/popular${ApiKey}`);
+    const resp =await axios.get(`${ApiUrl}/movie/popular?${ApiKey}`);
+    console.log(resp)
     return resp.data.results;
 };
 
 
-const getUpcomingMovies = async () =>{
-    const resp = await axios.get(`${ApiUrl}/3/movie/upcoming${ApiKey}`)
-}
