bd35d8bc65582708e1a6ec154d990be6e0d8276c
ubair <ubairnoor5@gmail.com>
2022-06-08 18:06:16 +0530

    SLider added

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,15 @@
 import    React,{ useState ,useEffect} from 'react';
-import { Text , View} from 'react-native';
+import { Text , View,StyleSheet} from 'react-native';
 import {getPopularMovies,getUpcommingMovies} from '../services/services'
 import { SliderBox } from "react-native-image-slider-box";
 const Home = () => {
 
-    const [ movieImages ,setMovieImages] =  useState(''); 
+    const [ movieImages ,setMovieImages] =  useState([]); 
 
     const [ error, setError] = useState(false);
     
     useEffect(()=>{
-      getUpcommingMovies().then(movies =>{
+      getUpcommingMovies().then((movies) =>{
         const moviesImagesArray = [];
         movies.forEach(movie =>{
           moviesImagesArray.push('https://image.tmdb.org/t/p/w500'+movie.poster_path)
@@ -27,10 +27,21 @@ const Home = () => {
     },[])
 
     return (
-        <View>
+        <View style={styles.sliderContainer}>
     <SliderBox images={movieImages} />
      </View>
+
     );
 }
+const styles = StyleSheet.create({
+sliderContainer:{
 
+display:'flex',
+justifyContent:'center',
+margin:5,
+paddingTop: 5,
+
+}
+
+});
 export default Home;
--- services/services.js
+++ services/services.js
@@ -3,15 +3,16 @@ import axios from 'axios';
 
 const ApiUrl = 'https://api.themoviedb.org/3'
 const ApiKey = 'api_key=283bf3f22f7ce6d2d1f9043a8b429b91'
+
+
 export  const getPopularMovies =  async () => {
     const resp =await axios.get(`${ApiUrl}/movie/popular?${ApiKey}`);
-    console.log(resp)
+    
     return resp.data.results;
 };
-
-
 export const getUpcommingMovies = async () =>{
     const resp = await axios.get(`${ApiUrl}/movie/upcoming?${ApiKey}`)
+    console.log("Resp of upcoming",resp.data.results)
     return resp.data.results;
 }
 
