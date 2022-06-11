841ee6fb1901b5dd07968c773e361fdaf0006f47
ubair <ubairnoor5@gmail.com>
2022-06-11 17:05:47 +0530

    Card{PlaceHolder Image added if th image is not avalable and Style added and Movie Name added}

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {TouchableOpacity, StyleSheet, Image} from 'react-native';
+import {TouchableOpacity, StyleSheet, Image,Text} from 'react-native';
+const placeholderimage = require('../assets/Images/placeholder.png')
 class Card extends React.PureComponent {
   state = ({item} = this.props);
   render() {
@@ -8,8 +9,11 @@ class Card extends React.PureComponent {
         <Image
         resizeMode='cover'
           style={styles.image}
-          source={{uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}}
+          source={
+              item.poster_path ?
+            {  uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path}: placeholderimage}
         />
+        {item.poster_path && <Text style={styles.movieName}>{item.title}</Text>}
       </TouchableOpacity>
     );
   }
@@ -18,12 +22,23 @@ const styles = StyleSheet.create({
   container: {
     padding: 5,
     position: 'relative',
+    alignItems:'center',
+    justifyContent:'center',
+    height:200
   },
   image: {
     height: 200,
     width: 120,
     borderRadius: 20,
   },
+  movieName:{
+      position:'absolute',
+      width:100,
+      textAlign:'center',
+      color:'white',
+      top:10,
+      
+  }
 });
 
 export default Card;
