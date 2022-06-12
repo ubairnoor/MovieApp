eb78a6831ca209af2d6e9b818cc2483fd69a1f30
ubair <ubairnoor5@gmail.com>
2022-06-12 20:17:42 +0530

    Promise Added

--- screens/Home.js
+++ screens/Home.js
@@ -6,93 +6,131 @@ import {
   Dimensions,
   dotStyle,
   FlatList,
-  ScrollView
+  ScrollView,
 } from 'react-native';
-import {getPopularMovies, getUpcommingMovies, getPopularTv,getFamilyMovie,getDocs} from '../services/services';
+import {
+  getPopularMovies,
+  getUpcommingMovies,
+  getPopularTv,
+  getFamilyMovie,
+  getDocs,
+} from '../services/services';
 import {SliderBox} from 'react-native-image-slider-box';
 import List from '../components/List';
 const dimensions = Dimensions.get('screen');
 
 const Home = () => {
-  const [movieImages, setMovieImages] = useState([]);
+  const [UpcomingMovies, setUpcomingMovies] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
-const [popularShows, setPopularTv] = useState([]);
-const [familyShow,setFamilyShow] = useState([])
-const [documentary,setDocumentary] = useState([])
+  const [popularShows, setPopularTv] = useState([]);
+  const [familyShow, setFamilyShow] = useState([]);
+  const [documentary, setDocumentary] = useState([]);
   const [error, setError] = useState(false);
 
+  const getData = () => {
+    return Promise.all([
+      getUpcommingMovies(),
+      getPopularMovies(),
+      getPopularTv(),
+      getFamilyMovie(),
+      getDocs(),
+    ]);
+  };
+
   useEffect(() => {
-    getUpcommingMovies()
-      .then(movies => {
-        const moviesImagesArray = [];
-        movies.forEach(movie => {
-          moviesImagesArray.push(
-            'https://image.tmdb.org/t/p/w500' + movie.poster_path,
-          );
-        });
-        setMovieImages(moviesImagesArray);
-      })
-      .catch(err => {
-        setError(err);
-      });
+    getData().then(([
+      UpcomingMovies,
+      popularMovies,
+      popularShows,
+      familyShow,
+      documentary,
+    ])=>{
+      const moviesImagesArray = [];
+      UpcomingMovies.forEach(movie => {
+        moviesImagesArray.push(
+          'https://image.tmdb.org/t/p/w500' + movie.poster_path,
+        );
 
-    getPopularMovies()
-      .then(movies => {
-        setPopularMovies(movies);
-      })
-      .catch(err => {
-        setError(err);
       });
-      getPopularTv().then(shows =>{
-        setPopularTv(shows);
-      })
-      .catch(err =>{
-        setError(err);
-      } )
-      //Get Family Show
-      getFamilyMovie().then(FamilyShow => {
-        setFamilyShow(FamilyShow)
-      }).catch(err=>{
-        setError(err)
-      })
-      getDocs().then( doc =>{
-        setDocumentary(doc)
-      }).catch(err =>{
-        setError(err)
-      })
+      setUpcomingMovies(moviesImagesArray);
+      setPopularMovies(popularMovies);
+      setPopularTv(popularShows);
+      setFamilyShow(familyShow);
+      setDocumentary(documentary);
+    }
+    ).catch(err=>{
+      setError(err);
+    });
+    // getUpcommingMovies()
+    //   .then(movies => {
+    //     const moviesImagesArray = [];
+    //     movies.forEach(movie => {
+    //       moviesImagesArray.push(
+    //         'https://image.tmdb.org/t/p/w500' + movie.poster_path,
+    //       );
+    //     });
+    //     setMovieImages(moviesImagesArray);
+    //   })
+    //   .catch(err => {
+    //     setError(err);
+    //   });
 
+    // getPopularMovies()
+    //   .then(movies => {
+    //     setPopularMovies(movies);
+    //   })
+    //   .catch(err => {
+    //     setError(err);
+    //   });
+    // getPopularTv()
+    //   .then(shows => {
+    //     setPopularTv(shows);
+    //   })
+    //   .catch(err => {
+    //     setError(err);
+    //   });
+    // //Get Family Show
+    // getFamilyMovie()
+    //   .then(FamilyShow => {
+    //     setFamilyShow(FamilyShow);
+    //   })
+    //   .catch(err => {
+    //     setError(err);
+    //   });
+    // getDocs()
+    //   .then(doc => {
+    //     setDocumentary(doc);
+    //   })
+    //   .catch(err => {
+    //     setError(err);
+    //   });
   }, []);
-
-
 
   return (
     <React.Fragment>
       <ScrollView>
-      <View style={styles.sliderContainer}>
-        <SliderBox
-          images={movieImages} 
-          sliderBoxHeight={dimensions.height / 2.4}
-          autoplay={true}
-          circleLoop={true}
-          dotStyle={styles.sliderStyle}
-        />
-      </View>
-      <View>
-        <List title="Popular Movies" content={popularMovies}/>
-      </View>
-      <View>
-        <List title="Popular Shows" content={popularShows}/>
-      </View>
-      <View>
-        <List  title="Documentary" content={documentary}/>
-      </View>
-      <View>
-        <List title="Family Shows" content={familyShow}/>
-      </View>
-      
+        <View style={styles.sliderContainer}>
+          <SliderBox
+            images={UpcomingMovies}
+            sliderBoxHeight={dimensions.height / 2.4}
+            autoplay={true}
+            circleLoop={true}
+            dotStyle={styles.sliderStyle}
+          />
+        </View>
+        <View>
+          <List title="Popular Movies" content={popularMovies} />
+        </View>
+        <View>
+          <List title="Popular Shows" content={popularShows} />
+        </View>
+        <View>
+          <List title="Documentary" content={documentary} />
+        </View>
+        <View>
+          <List title="Family Shows" content={familyShow} />
+        </View>
       </ScrollView>
-
-      
     </React.Fragment>
   );
 };
@@ -100,7 +138,6 @@ const styles = StyleSheet.create({
   sliderContainer: {
     display: 'flex',
     justifyContent: 'center',
-    
   },
   sliderStyle: {
     height: 0,
@@ -110,7 +147,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-
 });
 
 export default Home;
